a39d09d17c990ee4db04370f0b0bf6affbe7196c
Bachirm85 <mouawadbachir@gmail.com>
2021-05-25 21:47:13 +0300

    adding get data, validate data and show respond

--- home.php
+++ home.php
@@ -1,48 +1,59 @@
-<link rel="stylesheet" href="reg.css">
 
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reg.css">
+    <title>Home php</title>
+</head>
 <body>
 <div class="flx">
-<div class="container">
-<form class="reg_field" action="safe.php" method="post">
-<h2>Registration Form</h2>
+    <div class="container">
+        <form class="reg_field" action="safe.php" method="POST">
+        <h2>Registration Form</h2>
     
-        <label for="name">Full Name</label>
-        <input type="text" name="name" id="name"><br>
-        <label for="usrname">Username</label>
-        <input type="text" name="Username" id="Username"><br>
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" minlength="8" ><br>
-        <label for="cnfpass">Confirm Password</label>
-        <input type="password" name="cnfpass" id="confirm_password" ><br>
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email"><br>
-        <label for="phone">Phone</label>
-        <input type="tel" id="phone" name="phone"><br>
-        <label for="birthday">Date of Birth</label>
-        <input type="date" id="birthday" name="birthday"><br>
-        <label for="ssn">Social Security Number</label>
-        <input type="text" id="ssn" name="ssn"><br>
-        <button type="submit">Submit</button>
-        <label>
+            <label for="name">Full Name</label>
+            <input type="text" name="name" id="name"><br>
+            <label for="usrname">Username</label>
+            <input type="text" name="username" id="Username"><br>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" minlength="8" ><br>
+            <label for="cnfpass">Confirm Password</label>
+            <input type="password" name="cnfpass" id="confirm_password" ><br>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email"><br>
+            <label for="phone">Phone</label>
+            <input type="tel" id="phone" name="phone"><br>
+            <label for="birthday">Date of Birth</label>
+            <input type="date" id="birthday" name="birthday"><br>
+            <label for="ssn">Social Security Number</label>
+            <input type="text" id="ssn" name="ssn"><br>
+            <button type="submit" name="register">Submit</button>
+            <label>
     
-</form>
-</div>
+        </form>
+    </div>
 
-<div class="container">
-    <form action="safe.php" action="safe.php" method="post">
-    <h2>Login Form</h2>
+    <div class="container">
+        <form action="safe.php" method="POST">
+        <h2>Login Form</h2>
  
-    <label for="uname"><b>Username</b></label>
-    <input type="text" name="uname" required>
+            <label for="uname"><b>Username</b></label>
+            <input type="text" name="uname">
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" name="psw" required>
-        
-    <button type="submit">Login</button>
-    <label>
-</form>
+            <label for="psw"><b>Password</b></label>
+            <input type="password" name="psw">
+                
+            <button type="submit" name="login">Login</button>
+            <label>
+        </form>
 
-</div>
+    </div>
 </div>
 </body>
 
---
+++ safe.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+
+
+
+
+
+<?php
+
+if (isset($_POST["login"])) {
+
+    // get log in data
+    $username_log = $_POST["uname"];
+    $password_log = $_POST["psw"];
+
+    // Validate Data
+    if (!empty($username_log) && !empty($password_log)) {
+        if ($username_log == "Bachir" && $password_log == "Bmouawad") {
+            echo "Welcome Bachir";
+        } else {
+            echo "Please enter your correct username and password";
+        }
+
+    } else {
+        echo "Please enter your Username and Password";
+    }
+} elseif (isset($_POST["register"])) {
+
+    // Get register data
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $cnfpass = $_POST["cnfpass"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $birthday = $_POST["birthday"];
+    $ssn = $_POST["ssn"];
+
+    // validate data
+    if (!empty($username) && !empty($password) && !empty($cnfpass) && !empty($email) && !empty($phone) && !empty($birthday) && !empty($ssn)) {
+        if ($password && $cnfpass) {
+            if (is_numeric($phone) && is_numeric($ssn)) {
+                echo "Welcome";
+            } else {
+                echo "your phone and SSN should be numeric fields";
+            } 
+            
+        } else {
+            echo "Your passwords do not match";
+        }
+    } else {
+        echo "Please enter all fields";
+    }
+} else {
+    echo "Internal Error, please contact Support";
+} 
+
+?>
+
+</body>
+</html>
